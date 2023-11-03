dec7e59d445b4068e1c287524e8b20d74177baa1
amfield1 <57994822+amfield1@users.noreply.github.com>
2023-11-03 16:11:48 +0000

    Lab 4 - (Behind)
    -Need to finish Lab3
    -Currently on wk 6
    -Must start interrupts lab soon

--- Tasks/Task-330A-FuncOverload/main.cpp
+++ Tasks/Task-330A-FuncOverload/main.cpp
@@ -14,13 +14,13 @@ protected:
     double _real;
 
 public:
-    //Designated constructor
+    //Designated constructor - Constructor 1
     DoubleNumber(double r) {
         _real = r;
         cout << "This is the constructor of Base" << endl;
     }
 
-    //Convenience constructor
+    //Convenience constructor - Constructor 2
     DoubleNumber() : DoubleNumber(0.0) { }
 
     //Magnitude
@@ -38,6 +38,9 @@ public:
     void setValue(string strVal) {
         _real = stod(strVal);
     }
+    /*void setValue(DoubleNumber(DoubleNumber)){            //Task303A.6 - Incomplete
+        _real = DoubleNumber;
+    }*/
 
     double getValue() {
         return _real;
@@ -52,13 +55,15 @@ public:
 int main()
 {
     //Constructor Function overloading 
-    DoubleNumber n0(1.0);
+    DoubleNumber n0(1.0);               //The value of n1 is specified as "1.0" so the first constructor is called.
     DoubleNumber n1;
-    DoubleNumber n2;
+    DoubleNumber n2;                    //The values of n1 and n2 are not specified, so the second constructor is called to default their values to (0.0).
 
     //setValue function overloading  
-    n1.setValue(10);
-    n2.setValue("-3.0");
+    //n0.setValue(n1);                    //An error occurs trying to use this line, as their is no matching member function for call to "setValue"
+    n1.setValue(10);                    //The value called for n1 is noted as an integer, so the overload takes the "int" value and converts it to a double
+    n2.setValue("-3.0");                //The overload takes the string value and converts it to a double using the "stod" command
+    
 
     cout << n0.getValue() + n1.getValue() + n2.getValue() << endl;
 
